10003a80206af41d15514d4daae268db356a4efa
adrisoares <ninasoaresborges@gmail.com>
2024-01-05 13:17:57 +0000

    update code

--- pages/2_For Medical Professionals.py
+++ pages/2_For Medical Professionals.py
@@ -4,7 +4,10 @@ import pandas as pd
 import numpy as np
 import torch
 import os
+import subprocess
+import sys
 from PIL import Image
+import cv2
 
 st.set_page_config(
     page_title="For Medical Professionals",
@@ -36,35 +39,45 @@ content_2 = """
 """
 st.write(content_2, unsafe_allow_html=True)
 
-#### MODEL HERE ####
-default_file_path = "c:\Armazenamento\yolov5-master\data\img"
+#### For the Model in the CLOUD ####
 
-# Loading my model
 model_path = "C:/Armazenamento/yolov5-master/runs/train/exp36/weights/last.pt"
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path, force_reload=True)
 
-# Image Inference
+# Running model on an image
 def run_model(image_np):
     results = model(image_np)
     return results
 
-# Load the "unseen.png" image (just for demo). Ideally, this would be a path
-image_path = "Unseen_data.png"
-image = Image.open(image_path)
-image_np = np.array(image)
+# File buffer to cv2 image
+def create_opencv_image_from_stringio(img_stream, cv2_img_flag=1):
+    img_stream.seek(0)
+    img_array = np.asarray(bytearray(img_stream.read()), dtype=np.uint8)
+    return cv2.imdecode(img_array, cv2_img_flag)
 
-# Display the  image
-st.image(image, caption='Uploaded Image', use_column_width=True)
+# File uploader for a single image - Just for the demo
+img_file = st.file_uploader(label="Load Unseen_data.png",
+                             type=['png', 'jpg', 'jpeg'])
 
-if st.button("Run Model"):
-    results = run_model(image_np)
+if img_file:
+    open_cv_image = create_opencv_image_from_stringio(img_file)
+
+    # Display
+    st.image(open_cv_image, caption='Uploaded Image', use_column_width=True)
+
+    if st.button("Run Model"):
+        try:
+            results = run_model(open_cv_image)
+
+            # Display the results
+            st.success("Model has been run successfully!")
+            st.subheader("Detection Results:")
+            st.image(np.squeeze(results.render()), use_column_width=True)
+            st.image("recall.png")  
+        except Exception as e:
+            st.write(f"An error occurred: {e}")
+########
     
-    st.success("Model has been run successfully!")
-    st.subheader("Detection Results:")
-    st.image(np.squeeze(results.render()), use_column_width=True)
-    st.image("recall.png")
-#### MODEL HERE ####
-
 st.markdown("---") # Separator line
 st.markdown(f'<h1 style="color:#7E587E;font-size:36px;">{"We have come this far, but we want more!"}</h1>', unsafe_allow_html=True)
 
